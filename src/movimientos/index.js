42c22ae67324c48a78a5b200cfd15780f2ad78a0
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2024-08-26 12:51:09 -0300

    `Tarea JSON` | Tarea del 2 - readme. - reaorganizacion del repositorio. - listado de movimientos.

--- src/movimientos/index.js
+++ src/movimientos/index.js
@@ -1,15 +1,42 @@
-document.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Movimiento cargado con exito');
+const movimientos = [
+    { id: 1, descripcion: "Descripcion", categoria: 'Mercado', tipo: 1, monto: 100 },
+    { id: 2, descripcion: "Descripcion", categoria: 'Personal', tipo: 2, monto: 200 },
+    { id: 3, descripcion: "Descripcion", categoria: 'Transoporte', tipo: 3, monto: 300 },
+    { id: 4, descripcion: "Descripcion", categoria: 'Otro', tipo: 4, monto: 400 },
+]
 
-    const form = e.target;
-    let data = {};
-    for (let i = 0; i < form.elements.length; i++) {
-        const element = form.elements[i];
-        if (element.type != 'submit') {
-            data[element.name] = element.value;
-        }
-    }
+document.addEventListener('DOMContentLoaded', (e) => {
+    const container_movimientos = document.getElementById('movimientos');
+    let tabla = document.createElement('table');
+    let tblBody = document.createElement('tbody');
 
-    console.log(data);
-})
+    movimientos.forEach(m => {
+        let row = document.createElement('tr');
+        let celda_descripcion = document.createElement('td');
+        let celda_categoria = document.createElement('td');
+        let celda_tipo = document.createElement('td');
+        let celda_monto = document.createElement('td');
+
+        texto_descripcion = document.createTextNode(m.descripcion);
+        texto_categoria = document.createTextNode(m.categoria);
+        texto_tipo = document.createTextNode(m.tipo);
+        texto_monto = document.createTextNode(m.monto);
+
+        celda_descripcion.appendChild(texto_descripcion);
+        celda_categoria.appendChild(texto_categoria);
+        celda_tipo.appendChild(texto_tipo);
+        celda_monto.appendChild(texto_monto);
+
+        row.appendChild(celda_descripcion);
+        row.appendChild(celda_categoria);
+        row.appendChild(celda_tipo);
+        row.appendChild(celda_monto);
+
+        tblBody.appendChild(row);
+        
+    });
+
+    tabla.appendChild(tblBody);
+
+    document.body.appendChild(tabla);
+});
